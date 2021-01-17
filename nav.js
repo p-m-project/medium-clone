6223785071daa3b2faa429b48d4f549835ed44ff
prstwo <prs2.pari@gmail.com>
2021-01-17 13:46:34 +0330

    names changed/files added

---
+++ nav.js
@@ -0,0 +1,11 @@
+function showMenu(){
+                
+    if(document.querySelector(".topnav-sandwich-links").style.display=="none") {
+     document.querySelector(".topnav-sandwich-links").style.display="block";
+    
+    }
+    else{
+     document.querySelector(".topnav-sandwich-links").style.display="none";
+    }
+
+ }
