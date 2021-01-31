b466b22ba00e7c626ce68b9afb96d3f2b761b174
prstwo <prs2.pari@gmail.com>
2021-01-31 13:29:20 +0330

    new story page is almost working

--- js/new-post.js
+++ js/new-post.js
@@ -1,29 +1,64 @@
 
-
-          // document.querySelector(".new-post-passage").nextSibling.focus();
-
-          function function1(er){
-              er.addEventListener("keydown",(e)=>{
-                //create a global textarea element:
-                let pElement= document.createElement("textarea");
-                pElement.setAttribute("class", "new-post-passage");
-                pElement.setAttribute("contenteditable", "true");
-                pElement.setAttribute("onclick", "function1(this)")
-                //if users presses Enter, a new p element will be aded to element
-                if(e.keyCode===13){
-                        //document.querySelector(".new-post-container").appendChild(pElement).focus();  
-                        console.log()  
+/*function setClass(){
+    document.querySelector(".new-post-container").childNodes.setAttribute("class",".new-post-passage");
+}*/
+function nextElem(er){
+    er.addEventListener("keydown",(e)=>{      
+        if(e.keyCode===13){
+            er.nextElementSibling.focus();
                     } 
                })
           }
 
-/*document.querySelector(".new-post-container").activeElement.addEventListener("keydown",(e)=>{
-    //create a global textarea element:
-    let pElement= document.createElement("textarea");
-    pElement.setAttribute("class", "new-post-passage");
-    pElement.setAttribute("contenteditable", "true");
-    //if users presses Enter, a new p element will be aded to element
-    if(e.keyCode===13){
-            document.querySelector(".new-post-container").appendChild(pElement).focus();        
+/*function nextElem3(ew){
+    if(ew.keyCode===13){
+            //create a global p element:
+             getElem(elem);         
+            if(getE.nextElementSibling){
+                    getE.nextElementSibling.focus();
+                }
+            else{
+            let pEl= document.createElement("p");
+            pEl.setAttribute("contenteditable", "true");
+            pEl.setAttribute("onfocus", "getElem(this)");
+            pE1.setAttribute("onkeydown", "nextElem3(ew)");
+            document.querySelector(".new-post-container").appendChild(pEl).focus();  
+            addClassF();
+                }
+            
+            }          
+}*/
+
+function nextElem2(ka){
+    ka.addEventListener("keydown", (e)=>
+    {
+        if(e.keyCode===13){
+            //create a global p element:
+            if(ka.nextElementSibling){
+                ka.nextElementSibling.focus();
+            }
+            else{
+                let pElement= document.createElement("div");
+                pElement.setAttribute("contenteditable", "true");
+                pElement.setAttribute("class", "new-post-passage");
+                pElement.setAttribute("onfocus", "nextElem2(this)");
+                document.querySelector(".new-post-container").appendChild(pElement).focus();  
+            }          
+           // addClassF();
         } 
-})*/
+
+        if(e.keyCode===8){
+            //create a global p element:
+           if(ka.innerHTML===""){
+            ka.remove()
+           }
+           
+           // addClassF();
+        } 
+    })
+    
+
+}
+
+
+
