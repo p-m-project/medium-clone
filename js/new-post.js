efc7aa1f732f9d31df0bce99ed3a3c61916bbc61
prstwo <prs2.pari@gmail.com>
2021-02-01 22:30:56 +0330

    display image after choosing a picture

--- js/new-post.js
+++ js/new-post.js
@@ -1,6 +1,6 @@
-let btnAdd=document.querySelector(".button-add");
+let btnAdd=document.querySelector(".button-add-section");
 
-/*this function get dimension of the page in order to set the left value of emlement with class .button-add  */
+/*this function get dimension of the page in order to set the left value of emlement with class .button-add-section  */
 function getDimension(){
     let winWidth=window.innerWidth;
     let postContainerWidth=document.querySelector(".new-post-container").offsetWidth;
@@ -16,13 +16,17 @@ function nextElem(er){
                })
             }
 function nextElem3(ka){
-    /*get the distance og=f current element from top of the window:*/
+    /*get the distance of the current element from top of the window:*/
     let getHeight2=window.pageYOffset + ka.getBoundingClientRect().top;
     btnAdd.style.display="block";
     /*apply the height of the current element from top of the page as the 
         css top property:*/ 
-    btnAdd.style.display="block";
+    btnAdd.style.display="inline-block";
     btnAdd.style.top=`${getHeight2}px`;
+    //document.querySelectorAll(".new-post-passage").id='';
+    document.getElementById('txt').id="";
+    ka.id='txt';   
+
     ka.addEventListener("keydown", (e1)=>
     {
         
@@ -43,6 +47,7 @@ function nextElem3(ka){
                 pElement.setAttribute("class", "new-post-passage");
                 pElement.setAttribute("contenteditable", "true");
                 pElement.setAttribute("onfocus", "nextElem3(this)");
+                /*pElement.setAttribute("onblur", "removeId(this)");*/
                 document.querySelector(".new-post-container").appendChild(pElement).focus();  
             }          
            // addClassF();
@@ -56,13 +61,15 @@ function nextElem3(ka){
 }
 
 function nextElem2(k1){
-     /*get the distance og=f current element from top of the window:*/
+     /*get the distance of the current element from top of the window:*/
      let getHeight1=window.pageYOffset + k1.getBoundingClientRect().top;
-     btnAdd.style.display="block";
+     btnAdd.style.display="inline-block";
      /*apply the height of the current element from top of the page as the 
          css top property:*/ 
      btnAdd.style.display="block";
      btnAdd.style.top=`${getHeight1}px`;
+     document.getElementById('txt').id="";
+     k1.id='txt';
     k1.addEventListener("keydown", (e)=>
     {
        if(e.shiftKey){
@@ -83,6 +90,7 @@ function nextElem2(k1){
                 pElement.setAttribute("contenteditable", "true");
                 pElement.setAttribute("class", "new-post-passage");
                 pElement.setAttribute("onfocus", "nextElem3(this)");
+                /*pElement.setAttribute("onblur", "removeId(this)");*/
                 document.querySelector(".new-post-container").appendChild(pElement).focus();  
 
             }          
@@ -94,6 +102,30 @@ function nextElem2(k1){
 
 }
 
+/*function removeId(elem){
+    elem.id=""
+}*/
+
+function toggleAddMedia(){
+    if(document.querySelector(".add-media-section").style.display=="none"){
+        document.querySelector(".add-media-section").style.display="inline-block"
+    }
+    else{
+        document.querySelector(".add-media-section").style.display="none";
+    }
+}
+
+function loadImage(img){
+    var txt = document.getElementById('txt');
+    /* */
+    /* */
+    var imageElement = document.createElement('img')
+    txt.parentNode.insertBefore(imageElement,txt.nextSibling )
+    imageElement.src = URL.createObjectURL(img.target.files[0]);
+    imageElement.onload = function() {
+    URL.revokeObjectURL(imageElement.src) 
+    }
+}
 
 
 
