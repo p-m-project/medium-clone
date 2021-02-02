092aec21e0a2ff8f1057edc2fe35aa427c1bb288
Masoume Vesali <smart.yas.sbu@gmail.com>
2021-02-02 20:09:54 +0330

    add buttom delet for article

--- js/nav.js
+++ js/nav.js
@@ -30,3 +30,25 @@ function showMenu(){
         }
         document.getElementById(cityName).style.display = "block";  
         }
+
+// button
+$("button").click(function(){
+	if($(this).hasClass("confirm")){
+		$(this).addClass("done");
+		$("span").text("Deleted");
+	} else {
+		$(this).addClass("confirm");
+		$("span").text("Are you sure?");
+	}
+});
+
+// Reset
+$("button").on('mouseout', function(){
+	if($(this).hasClass("confirm") || $(this).hasClass("done")){
+		setTimeout(function(){
+			$("button").removeClass("confirm").removeClass("done");
+			$("span").text("Delete");
+		}, 3000);
+	}
+});
+
