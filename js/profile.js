d12408532a16460380169032130f362508345c37
Masoume Vesali <smart.yas.sbu@gmail.com>
2021-02-11 23:04:18 +0330

    add part

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,9 @@
-thumbnailElement.src = URL.createObjectURL(thumbnail.target.files[0]);
-    thumbnailElement.onload = function() {
-    URL.revokeObjectURL(thumbnailElement.src) 
-    } 
+
+
+
+// thumbnailElement.src = URL.createObjectURL(thumbnail.target.files[0]);
+//     thumbnailElement.onload = function() {
+//     URL.revokeObjectURL(thumbnailElement.src) 
+//     } 
     // هر پارت رو تعریف کنم
     // المان رو باید هدف قرار بدم
