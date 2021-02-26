5b6a082140a036c6c54c7030a9cbb93a41b93ead
prstwo <prs2.pari@gmail.com>
2021-02-26 15:58:07 +0330

    save buttons become interactive/ header image added

--- js/home-page.js
+++ js/home-page.js
@@ -9,3 +9,14 @@ for(i=0;i<followBtn.length;i++){
         }
     })
 }
+let saveBtn=document.querySelectorAll('.fa-save');
+for(i=0;i<saveBtn.length;i++){
+    saveBtn[i].addEventListener('click',function(e1){
+        if(e1.target.style.color==='black'){
+            e1.target.style.color='inherit'
+        }
+        else{
+            e1.target.style.color='black'
+        }
+    })
+}
