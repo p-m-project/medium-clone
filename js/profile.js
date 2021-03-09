66440521a482b84e2ca7eba9ed249908e82f0852
prstwo <prs2.pari@gmail.com>
2021-03-09 13:32:07 +0330

    some edits on massome codes in profile pages-fixed div as profile-top added-some interactivity and js codes added

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,14 @@
+function scrollTop(){
+  if(document.body.scrollTop > 76 || document.documentElement.scrollTop > 76){
+    document.querySelector('.profile-top').style.top='0';
+    document.querySelector('.profile-top').style.backgroundColor="rgba(211, 211, 211,.95)";
+  }
+  else{
+    document.querySelector('.profile-top').style.top='';
+    document.querySelector('.profile-top').style.backgroundColor="";
+  }
+}
+window.onscroll=function(){scrollTop()};
 function readURL(input) {
     if (input.files && input.files[0]) {
       var reader = new FileReader();
@@ -13,5 +24,3 @@ function readURL(input) {
   $("#imgInp").change(function() {
     readURL(this);
   });
-
-//   *********************part periview image
